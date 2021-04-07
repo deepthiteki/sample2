d8e46e2bafb89070cd093ca2284b930229a9a121
Deepthi Teki <deepthi.teki09@gmail.com>
2021-04-07 18:28:19 +0530

    all files

---
+++ ninitialization.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+int main()
+{
+    int n,arr[100];
+    cout<<"Enter the value of n";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    arr[i]=i;
+    print(arr);
+    return 0;
+}
